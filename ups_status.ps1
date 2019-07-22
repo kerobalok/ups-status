11b49a26d5d024e9a4c8ebc9345fca962071301e
Daniel Sajdyk <daniel.sajdyk@lezajsk.sr.gov.pl>
2019-07-22 21:57:08 +0200

    First working version. It logs data to screen and to a file.

--- ups_status.ps1
+++ ups_status.ps1
@@ -1,74 +1,131 @@
-﻿$computer = '.'
+﻿[CmdletBinding()]
+param()
+
+<################################## -VARIABLES- #####################################>
+$log_file = "$PsScriptRoot\log.csv" # the name and path of the log file. Deafult is the path withing script directory with name "log.csv"
+$log_data = $null # variable that stores information about the selection of the user about whether the script should log data to the file or not
+<################################## -FUNCTIONS- #####################################>
+
+function log_to_file ($content){
+ 
+    if ($(($log_data -eq "y") -or ($log_data -eq "yes"))){
+        Write-Verbose "Funkcja 'log_to_file' zapisuje dane do pliku"
+        Add-Content $log_file $content
+    }
+    else {Write-Verbose "Zmienna log_data ma wartosc $log_data, a wiec funkcja 'log_to_file' nie zapisuje danych do pliku"}
+}
+
+<################################## -QUESTIONS- #####################################>
 $dlugosc_pomiaru = Read-host -Prompt "- podaj długość testu w minutach"
-$dlugosc_pomiaru = new-timespan -Minutes $dlugosc_pomiaru 
-$interwal_odczytow = Read-host -Prompt "- podaj interwał odczytywania informacji w sekundach" 
+$dlugosc_pomiaru = new-timespan -Minutes $dlugosc_pomiaru
+$reading_interval = Read-host -Prompt "- podaj interwał odczytywania informacji w sekundach"
 
-<########################### BASIC INFORMATIONS ABOUT UPS ###########################>
-Write-Host "Name: " (Get-WmiObject -class Win32_Battery -ComputerName $computer -NameSpace root\CIMV2 | select -ExpandProperty "Name") -ForegroundColor Green
+<######################## -SHOULD DATA BE LOGGED TO A FILE?- ########################>
+DO
+{
+    $log_data = Read-Host -Prompt "- should data be logged to a file (y/n)"
+    if ($($log_data -ne "y") -and $($log_data -ne "yes") -and $($log_data -ne "n") -and $($log_data -ne "no")){
+        Write-Warning -Message "'$log_data' is not valid option. Please choose (y/n)";
+    }
+} until ($($log_data -eq "y") -or $($log_data -eq "yes") -or $($log_data -eq "n") -or $($log_data -eq "no"))
 
-$chemistry = switch (Get-WmiObject -Class Win32_Battery -Namespace root\CIMV2 | select -ExpandProperty "Chemistry")
+switch ($log_data)
+{
+    {($_ -eq "y") -or ($_ -eq "yes")} {
+        if (!(Test-Path $log_file)){
+                New-Item -path $log_file -ItemType "file"
+                Write-Information "New file was created in: $log_file" -InformationAction Continue
+            }
+        else {Write-Information "Logging enabled to file $log_file" -InformationAction Continue}
+        }
+    {($_ -eq "n") -or ($_ -eq "no")} {Write-Information "Logging disabled" -InformationAction Continue} 
+}
+
+
+
+<########################## -BASIC INFORMATIONS ABOUT UPS- ##########################>
+Write-Host "`r`n-- Basic informations"
+######Write-Host"-- Basic informations" 
+log_to_file ("-- Basic informations")
+
+Write-Host "Name: $(Get-CimInstance -ClassName Win32_Battery -Namespace root\CIMV2 | Select-Object -ExpandProperty Name)"
+#######Write-Host"Name: $(Get-CimInstance -ClassName Win32_Battery -Namespace root\CIMV2 | Select-Object -ExpandProperty Name)" 
+log_to_file ("Name: $(Get-CimInstance -ClassName Win32_Battery -Namespace root\CIMV2 | Select-Object -ExpandProperty Name)")
+
+$chemistry = switch (Get-CimInstance -ClassName Win32_Battery -Namespace root\CIMV2 | Select-Object -ExpandProperty "Chemistry")
     {
-        1 {Write-Host "Chemistry: Other"}
-        2 {Write-Host "Chemistry: Unknown"}
-        3 {Write-Host "Chemistry: Lead Acid" -ForegroundColor Green}
-        4 {Write-Host "Chemistry: Nickel Cadmium"}
-        5 {Write-Host "Chemistry: Nickel Metal Hydride"}
-        6 {Write-Host "Chemistry: Lithium-ion"}
-        7 {Write-Host "Chemistry: Zinc air"}
-        8 {Write-Host "Chemistry: Lithium Polymer"}
-        default {Write-Host "Chemistry: can not determine the type of battery" -ForegroundColor Red}
+        1 {Write-Host "Chemistry: Other" 
+            log_to_file ("Chemistry: Other")}
+        2 {Write-Host "Chemistry: Unknown"
+            log_to_file ("Chemistry: Unknown")}
+        3 {Write-Host "Chemistry: Lead Acid"
+            log_to_file ("Chemistry: Lead Acid")}
+        4 {Write-Host "Chemistry: Nickel Cadmium" 
+            log_to_file ("Chemistry: Nickel Cadmium")}
+        5 {Write-Host "Chemistry: Nickel Metal Hydride"
+            log_to_file ("Chemistry: Nickel Metal Hydride")}
+        6 {Write-Host "Chemistry: Lithium-ion"
+            log_to_file ("Chemistry: Lithium-ion")}
+        7 {Write-Host "Chemistry: Zinc air"
+            log_to_file ("Chemistry: Zinc air")}
+        8 {Write-Host "Chemistry: Lithium Polymer"
+            log_to_file ("Chemistry: Lithium Polymer")}
+        default {Write-Warning -Message "Can not determine the type of battery (Chemistry parameter)"
+            log_to_file ("Can not determine the type of battery (Chemistry parameter)")}
     }
 
 
 
 
 <########################### TABLE WITH MEASURED UPS VALUES ###########################>
-# to jest chyba tablica
-$ups = @{
-    data = Get-Date -Format "dd.MM.yyyy"
-    godzina = Get-Date -Format "HH:mm:ss"
-    Availability = (Get-WmiObject -Class Win32_Battery -ComputerName $computer -Namespace root\CIMV2 | select -ExpandProperty "Availability")
-    BatteryStatus = (Get-WmiObject -class Win32_Battery -ComputerName $computer -NameSpace root\CIMV2 | select -ExpandProperty "BatteryStatus" ) 
-    EstimatedChargeRemaining = (Get-WmiObject -class Win32_Battery -ComputerName $computer -NameSpace root\CIMV2 | select -ExpandProperty "EstimatedChargeRemaining" ) 
-    EstimatedRunTime = (Get-WmiObject -class Win32_Battery -ComputerName $computer -NameSpace root\CIMV2 | select -ExpandProperty "EstimatedRunTime")
-    
-}
+Write-Host "-- measurements started"
+log_to_file ("-- measurements started")
 
-# z powyższej tablicy tworzę obiekt, który będzie przechowywał parametry UPSa
-$obiekt = New-Object psobject -Property $ups; 
+
+# Tworzę obiekt, który będzie przechowywał parametry UPSa
+$obiekt = New-Object psobject -Property @{
+    date = Get-Date -Format "dd.MM.yyyy"
+    time = Get-Date -Format "HH:mm:ss"
+    Availability = (Get-CimInstance -ClassName Win32_Battery -Namespace root\CIMV2 | Select-Object -ExpandProperty "Availability")
+    BatteryStatus = (Get-CimInstance -ClassName Win32_Battery -Namespace root\CIMV2 | Select-Object -ExpandProperty "BatteryStatus")
+    EstimatedChargeRemaining = (Get-CimInstance -ClassName Win32_Battery -Namespace root\CIMV2 | Select-Object -ExpandProperty "EstimatedChargeRemaining")
+    EstimatedRunTime = (Get-CimInstance -ClassName Win32_Battery -Namespace root\CIMV2 | Select-Object -ExpandProperty "EstimatedRunTime")
+};
 
 #wyświetlam obiekt pipeując go przez format tabeli oraz funkcję trim dzięki czemu usuwane są puste wiersze przed danymi i po nich. Po prostu tabelka się ładniej wyświetla
-($obiekt | Format-Table -Property Data, Godzina, Availability, BatteryStatus, EstimatedChargeRemaining, EstimatedRunTime | Out-String).Trim() ;
-start-sleep -seconds $interwal_odczytow
+($obiekt | Format-Table -Property date, time, Availability, BatteryStatus, EstimatedChargeRemaining, EstimatedRunTime | Out-String).Trim();
+log_to_file ("date`ttime`tAvailability`tBatteryStatus`tEstimatedChargeRemaining`tEstimatedRunTime")
+log_to_file ($((($obiekt.date + "`t" +  $obiekt.time + "`t" + $obiekt.Availability + "`t" + $obiekt.BatteryStatus + "`t" + $obiekt.EstimatedChargeRemaining + "`t" + $obiekt.EstimatedRunTime | Out-String).Trim())))
+
+
+start-sleep -seconds $reading_interval
 
 $sw = [diagnostics.stopwatch]::StartNew()
 
 while ($sw.elapsed -lt $dlugosc_pomiaru){
-    $obiekt.data = Get-Date -Format "dd.MM.yyyy"
-    $obiekt.godzina = Get-Date -Format "HH:mm:ss"
-    $obiekt.Availability = Get-WmiObject -Class Win32_Battery -ComputerName $computer -Namespace root\CIMV2 | select -ExpandProperty "Availability"
-    $obiekt.BatteryStatus = (Get-WmiObject -class Win32_Battery -ComputerName $computer -NameSpace root\CIMV2 | select -ExpandProperty "BatteryStatus" ) 
-    
-    $obiekt.EstimatedChargeRemaining = Get-WmiObject -class Win32_Battery -ComputerName $computer -NameSpace root\CIMV2 | select -ExpandProperty "EstimatedChargeRemaining"
-    
-    $Current_EstimatedRunTime = Get-WmiObject -class Win32_Battery -ComputerName $computer -NameSpace root\CIMV2 | select -ExpandProperty "EstimatedRunTime"
+    $obiekt.date = Get-Date -Format "dd.MM.yyyy"
+    $obiekt.time = Get-Date -Format "HH:mm:ss"
+    $obiekt.Availability = (Get-CimInstance -ClassName Win32_Battery -Namespace root\CIMV2 | Select-Object -ExpandProperty "Availability")
+    $obiekt.BatteryStatus = (Get-CimInstance -ClassName Win32_Battery -Namespace root\CIMV2 | Select-Object -ExpandProperty "BatteryStatus")
+    $obiekt.EstimatedChargeRemaining = (Get-CimInstance -ClassName Win32_Battery -Namespace root\CIMV2 | Select-Object -ExpandProperty "EstimatedChargeRemaining")
+
+    $Current_EstimatedRunTime = (Get-CimInstance -ClassName Win32_Battery -Namespace root\CIMV2 | Select-Object -ExpandProperty "EstimatedRunTime")
     if (($Current_EstimatedRunTime) -eq 71582788) {
         $Current_EstimatedRunTime = "AC powered"
         $obiekt.EstimatedRunTime = $Current_EstimatedRunTime
     }
     else {
         $obiekt.EstimatedRunTime = $Current_EstimatedRunTime
-    }
-    
+    }   
 
-    
-    
 
     #wyświetlam utworzony obiekt przepuszczając go przez formatowanie tabeli oraz powstały strin puszczam do funkcji TRIM w celu usunięcia pustych wierszy
-    ($obiekt | Format-Table -HideTableHeaders -Property Data, Godzina,  Availability, BatteryStatus, EstimatedChargeRemaining, EstimatedRunTime | Out-String).Trim() ;
-     
+    ($obiekt | Format-Table -HideTableHeaders -Property date, time,  Availability, BatteryStatus, EstimatedChargeRemaining, EstimatedRunTime | Out-String).Trim();
+    log_to_file ($((($obiekt.date + "`t" +  $obiekt.time + "`t" + $obiekt.Availability + "`t" + $obiekt.BatteryStatus + "`t" + $obiekt.EstimatedChargeRemaining + "`t" + $obiekt.EstimatedRunTime | Out-String).Trim())))
+
     #tutaj ustawia się jaka ma być przerwa pomiędzy pomiarami
-    start-sleep -seconds $interwal_odczytow
+    start-sleep -seconds $reading_interval
 }
 
-Write-Host "- measurement completed"
+Write-Host "-- measurements completed"
+log_to_file ("-- measurements completed")
